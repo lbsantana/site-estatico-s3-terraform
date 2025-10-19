3e78336e96c2ce754604a0cc4951cceace424ecf
Lucas Barbosa <lucaslukibs@gmail.com>
2025-10-18 21:33:38 -0300

    uploading frontend static-site

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        secondary: "#f9fafb",
+      },
+    },
+  },
+  plugins: [],
+}
